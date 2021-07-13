5a10439edecbb5296646deac5105b7a235b8740f
Karthikeyan <lkarthik76@gmail.com>
2021-07-13 21:59:30 +0530

    Update program2.java

--- program2.java
+++ program2.java
@@ -3,6 +3,21 @@ package test;
 import java.util.ListIterator;
 import java.util.Vector;
 
+/*
+ * 
+ *  Find the list of risky cities to drive
+ *  Sample file
+ *  2 - data for two cities
+ *  1,5,10 - city 1, 5 drivers, max possible score in 10
+ *  4,8,5,9,9 - driving scores for 5 drivers of city 1
+ *  2,6,15  - city 2, 6 drivers, max possible score in 15
+ *  11,14,14,10,9,12 - driving score for 6 drivers of city 2
+ * 
+ *  1) A risky city is where more than 60% of the drivers score below an input safety percentage of max score
+ *  2) in given sample if input safety percentage is 81, for city 1,3 drivers have scored less than safety percentage score 
+ *  of 8. 3 out of 5 drivers scored less than safty percentage.
+ */
+
 public class program2 {
 /**
  * @author karthikeyan
